163a636d1739845267ddfb24771eb7e502be7371
Shinya0628 <k.shinya.ipad.777@gmail.com>
2020-11-04 14:07:38 -0800

    Complete

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log(err));
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
